0011130561824fa181abbcfcf4eab9eb07a63e0c
Jerry Moua <jerrymoua25@gmail.com>
2019-07-17 18:09:54 -0500

    nav bar 1

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -15,24 +15,35 @@ function Nav() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="#">Bet Type <span className="sr-only">(current)</span></a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Streak Type</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Favorite Bets</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" dataToggle="dropdown" ariaHaspopup="true" ariaExpanded="false">
-            Dropdown
-          </a>
-          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <a className="nav-link" href="#"><span className="sr-only">(current)</span></a>
+          <DropdownButton id="dropdown-basic-button" title="Bet Type">
+  <Dropdown.Item href="#/action-1">Single Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">Double Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Quad Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">1/3 Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Black Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Red Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Even Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Odd Bet</Dropdown.Item>
 </DropdownButton>
-          
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"></a>
+          <DropdownButton id="dropdown-basic-button" title="Streak Type">
+  <Dropdown.Item href="#/action-1">Hottest Five Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">Hottest 10 Bet</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Due to Hit</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Even/ Odds</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Hits on Black or Red</Dropdown.Item>
+</DropdownButton>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#"></a>
+          <DropdownButton id="dropdown-basic-button" title="Favorite Bet">
+  <Dropdown.Item href="#/action-1">Favorite Number </Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Favorite 10 Number</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">Favorite Color</Dropdown.Item>
+</DropdownButton>
         </li>
       </ul>
     </div>
